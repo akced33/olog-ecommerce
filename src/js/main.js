b555f972348de0b1d143e1d333f0901023e17c9f
akced33 <107038908+akced33@users.noreply.github.com>
2023-02-23 14:39:47 +0100

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,3 @@
-  //Using cookie presence to avoid tracking an employee. Only capture sessions when the cookie is not present
-$(document).ready(console.log('ready'));
-
-$(document).ready(function () {
-  console.log('here')
-  if (document.cookie.split(";").some((item) => item.trim().startsWith("employee="))) {
-    console.log('The cookie "employee" exists');
-  } else {
-    console.log('no "employee" cookie');
-  }
-});
-
 $(document).ready(function () {
   
   // Sticky Menu
