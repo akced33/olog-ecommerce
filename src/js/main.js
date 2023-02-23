e6b3f11a4ee6f4aa55fd48966ca4fbfa2aebdebc
akced33 <107038908+akced33@users.noreply.github.com>
2023-02-23 14:36:50 +0100

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,6 @@
   //Using cookie presence to avoid tracking an employee. Only capture sessions when the cookie is not present
+$(document).ready(console.log('ready'));
+
 $(document).ready(function () {
   console.log('here')
   if (document.cookie.split(";").some((item) => item.trim().startsWith("employee="))) {
